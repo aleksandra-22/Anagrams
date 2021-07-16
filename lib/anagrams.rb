d9743af32e8cc7dcf37142efe4bb87ac4d905e5e
aleksandra-22 <aleksandra.katic99@gmail.com>
2021-07-16 13:11:45 +0200

    Create show method for presentation

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -23,4 +23,11 @@ class Anagrams
     end
     result
   end
+
+  def show(input_text)
+    an = Anagrams.new(input_text)
+    an.get_anagrams.values.select {|word_values| word_values.length > 1}.each do |words|
+      puts words.join(", ")
+    end
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -3,7 +3,4 @@ require_relative "anagrams"
 print "Enter the name of the file you want to check for anagrams: "
 input_text = gets.chomp
 print "Anagrams from your #{input_text} file are: \n"
-an = Anagrams.new(input_text)
-an.get_anagrams.values.select {|word_values| word_values.length > 1}.each do |words|
-  puts words.join(", ")
-end
+Anagrams.new(input_text).show(input_text)
