b3c88982d5c62c8b2d53586be4a6384903a7e612
aleksandra-22 <aleksandra.katic99@gmail.com>
2021-07-16 12:24:03 +0200

    fix indentation

--- lib/main.rb
+++ lib/main.rb
@@ -5,5 +5,5 @@ input_text = gets.chomp
 print "Anagrams from your #{input_text} file are: \n"
 an = Anagrams.new(input_text)
 an.get_anagrams.values.select {|word_values| word_values.length > 1}.each do |words|
-	puts words.join(", ")
+  puts words.join(", ")
 end
