5884ecc318cc5d0f6d1b9d45d3967aea7e163041
aleksandra-22 <aleksandra.katic99@gmail.com>
2021-08-06 14:42:39 +0200

    change rspec names to match with new method

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,17 +6,12 @@ RSpec.describe Anagrams do
       an = Anagrams.new("test.txt")
         expect(an.show).to eql([["boaster", "boaters"], ["paste", "pates"], ["fresher", "refresh"]])
     end
-
-    it "returns all anagrams from an array" do
-      an = Anagrams.new(["boaters", "boaster", "paste", "pates"])
-        expect(an.show).to eql([["boaters", "boaster"], ["paste", "pates"]])
-    end
   end
 
-  describe '#get_word' do
+  describe '#get_anagrams' do
     it "returns all words that are anagrams with the word 'paste' from given file" do
       an = Anagrams.new("anagrams-wordlist.txt")
-      expect(an.get_word('paste')).to eql(["paste", "pates", "peats", "septa", "spate", "tapes", "tepas"])
+      expect(an.get_anagrams('paste')).to eql(["paste", "pates", "peats", "septa", "spate", "tapes", "tepas"])
     end
   end
 end
